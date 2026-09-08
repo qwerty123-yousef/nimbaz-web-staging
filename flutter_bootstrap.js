05aed249dcd8dbeb35ba6957957e8a4d40af39c2
Yousef <rezaii5223@gmail.com>
2026-09-08 21:11:26 +0330

    staging: 1.2.16 — Sentry disabled on web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -48,6 +48,6 @@ _flutter.loader.load({
     canvasKitBaseUrl: "canvaskit/"
   },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1587035918" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2849684254" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
