5bb0dcc50a9c44968ef4787dbba0cbd8c8679f84
Yousef <rezaii5223@gmail.com>
2026-09-08 18:49:03 +0330

    staging: 1.2.13 — new gradient icon + deactivated-account message

--- main.dart.js
+++ main.dart.js
@@ -58962,7 +58962,7 @@ q.push(A.cG(A.eD(B.u0,B.H4,A.bcL(),A.Ay(r,r,r,r,r,r,r,r,r,B.h7,r,r,r,r,r,B.iB,r,
 B.c.N(p,q)}p.push(B.a5)
 p.push(A.cG(A.eD(B.u8,B.GX,new A.aOg(n,a),r),r,1/0))
 p.push(B.bI)
-p.push(new A.ea(B.Q,r,r,A.a4("\u0646\u06cc\u0645\u200c\u0628\u0627\u0632 \u2014 \u0646\u0633\u062e\u0647 1.2.12",r,r,r,A.ct(r,r,B.aY,r,r,r,r,r,r,r,r,12,r,r,B.az,r,r,!0,r,r,r,r,r,r,r,r),r,r),r))
+p.push(new A.ea(B.Q,r,r,A.a4("\u0646\u06cc\u0645\u200c\u0628\u0627\u0632 \u2014 \u0646\u0633\u062e\u0647 1.2.13",r,r,r,A.ct(r,r,B.aY,r,r,r,r,r,r,r,r,12,r,r,B.az,r,r,!0,r,r,r,r,r,r,r,r),r,r),r))
 p.push(B.a5)
 p.push(new A.ea(B.Q,r,r,A.bP(A.a4("\u062d\u0630\u0641 \u062d\u0633\u0627\u0628",r,r,r,A.ct(r,r,B.cp,r,r,r,r,r,r,r,r,11,r,r,r,r,r,!0,r,r,r,r,r,r,r,r),r,r),new A.aOh(n,a),r),r))
 p.push(B.bI)
@@ -59754,7 +59754,7 @@ if(p.r)s=B.iO
 else{s=p.CW
 r=s>0
 q=r?o:p.gaCm()
-s=A.dr(A.a4(r?"\u0627\u0631\u0633\u0627\u0644 \u062f\u0648\u0628\u0627\u0631\u0647 ("+s+" \u062b\u0627\u0646\u06cc\u0647 \u062f\u06cc\u06af\u0631)":"\u062f\u0631\u06cc\u0627\u0641\u062a \u06a9\u062f \u062a\u0627\u06cc\u06cc\u062f",o,o,o,o,o,o),q,o)}return A.b([n,B.bU,l,B.p9,s,B.a6C,A.eo(A.a4("\u0646\u06cc\u0645\u200c\u0628\u0627\u0632 \u2014 \u0646\u0633\u062e\u0647 1.2.12",o,o,o,A.ct(o,o,B.cp,o,o,o,o,o,o,o,o,12,o,o,o,o,o,!0,o,o,o,o,o,o,o,o),o,o),o,o)],m)}}
+s=A.dr(A.a4(r?"\u0627\u0631\u0633\u0627\u0644 \u062f\u0648\u0628\u0627\u0631\u0647 ("+s+" \u062b\u0627\u0646\u06cc\u0647 \u062f\u06cc\u06af\u0631)":"\u062f\u0631\u06cc\u0627\u0641\u062a \u06a9\u062f \u062a\u0627\u06cc\u06cc\u062f",o,o,o,o,o,o),q,o)}return A.b([n,B.bU,l,B.p9,s,B.a6C,A.eo(A.a4("\u0646\u06cc\u0645\u200c\u0628\u0627\u0632 \u2014 \u0646\u0633\u062e\u0647 1.2.13",o,o,o,A.ct(o,o,B.cp,o,o,o,o,o,o,o,o,12,o,o,o,o,o,!0,o,o,o,o,o,o,o,o),o,o),o,o)],m)}}
 A.aQ6.prototype={
 $0(){this.a.CW=this.b},
 $S:0}
