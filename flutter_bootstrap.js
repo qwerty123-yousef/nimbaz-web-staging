0e4a437a3e84ce106c3a543cc8a359063d543177
Yousef <rezaii5223@gmail.com>
2026-09-08 16:11:59 +0330

    staging: 1.2.9 — instant oversized-photo reject + downscale before crop

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"5d531788691ec3404cac0cee66ead4007b1773
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2487125655" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "610838399" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
